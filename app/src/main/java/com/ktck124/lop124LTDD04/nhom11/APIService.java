8f1a266a71d2bcc46501920a97973332ae6044c2
bichngoc61204 <bichngoc61204@gmail.com>
2024-12-21 10:36:36 +0700

    api

---
+++ app/src/main/java/com/ktck124/lop124LTDD04/nhom11/APIService.java
@@ -0,0 +1,10 @@
+package com.ktck124.lop124LTDD04.nhom11;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface APIService {
+
+}
